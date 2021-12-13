b5d25ed717a01efbe85b8880980e6ba9979d299a
volhashax <volhashax@gmail.com>
2021-12-13 21:45:00 +0300

    initial

---
+++ ITAcademy/Integral/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integral
+{
+    internal class Calculator
+    {
+        public double CalculateIntegral(Func<double, double> func)
+        {
+            var sInterval = 0d;
+            // for (int i = 10; i < 2; i = i + 10)
+            {
+              sInterval = func(4);
+            }
+            return sInterval;
+        }
+    }
+}
---
+++ ITAcademy/Integral/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Integral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите функцию для вычисления с параметром x");
+
+            var str = Console.ReadLine();
+            try
+            {
+                var x = Expression.Parameter(typeof(double), "x");
+                var expression = ParseFormula.DynamicExpression.ParseLambda(new ParameterExpression[] {x}, typeof(double), str);
+
+                var func = (Func<double, double>)expression.Compile();
+
+                var calc = new Calculator();
+
+                var y = calc.CalculateIntegral(func);
+
+                Console.Write($"result f(2) = {y}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
